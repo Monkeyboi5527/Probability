d06476beea99b3a91bd003710aa6384bfa44a2bd
MonkeyBoi <monkeylaguri308@gmail.com>
2026-07-05 21:24:31 -0400

    test commit

--- src/Probability/Main.java
+++ src/Probability/Main.java
@@ -2,5 +2,5 @@
 /// A project meant to learn how to take in data and predict what will come next
 void main() {
 
-    System.out.println("Hello World");
+    System.out.println("Hello World"); // test
 }
