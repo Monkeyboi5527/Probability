8151941903d60070c98265e1a0f6b77adba1eaa1
MonkeyBoi <monkeylaguri308@gmail.com>
2026-07-06 23:07:23 -0400

    Coin Flip

--- src/Probability/Main.java
+++ src/Probability/Main.java
@@ -2,5 +2,22 @@
 /// A project meant to learn how to take in data and predict what will come next
 void main() {
 
-    System.out.println("Hello World"); // test
+   Random random = new Random();
+   int[] flips = flipper(random, 100);
+   int f = (int) Arrays.stream(flips).filter(flip -> flip == 0).count();
+   System.out.println(Arrays.toString(flips));
+   System.out.println(probability(f,flips.length));
+   
 }
+
+    public int[] flipper(Random random, int n) {
+        return IntStream.range(0, n).map(i -> coinFlip(random)).toArray();
+    }
+
+    public int coinFlip(Random random) {
+        return random.nextInt(0,2);
+    }
+    
+    public float probability(float a, float b) {
+        return a/b;
+    }
